6d5f150fa2610bd63f3526bfa16aa71fb2fca211
Saket Kamble <saketskamble@gmail.com>
2023-02-02 10:22:13 +0530

    added method to demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,12 @@
-class Demo{
+class Demo {
 
-public static void main(String args[]){
-System.out.println("hello");
-}
+    public static void main(String args[]) {
+
+        System.out.println("hello");
+        Demo.m1();
+    }
+    public static void m1(){
+        System.out.println("new method created");
+
+    }
 }
